d97c9d91ac9bc42b5cfcce1e8cb9143c7db58b96
mohammadjavadpirhadi <mohammadjavad.pirhadi@gmail.com>
2025-01-10 18:05:53 +0330

    fix(bash_scripts): Fix bash scripts

--- dvc1_train.sh
+++ dvc1_train.sh
@@ -11,3 +11,6 @@ python train.py --config dvc1_tv_sml-60_gop-a_Ife-clip_ur-0_ut-1_pfe-resp_a-90.j
 python train.py --config dvc1_tv_sml-40_gop-a_Ife-clip_ur-0_ut-1_pfe-resp_a-90.json
 
 python train.py --config dvc1_tv_sml-60_gop-60_Ife-clip_ur-0_ut-1_pfe-resp_a-90.json
+
+python train.py --config dvc1_tv_sml-60_gop-a_Ife-clip_ur-0_ut-1_pfe-resp_a-40.json
+python train.py --config dvc1_tv_sml-60_gop-a_Ife-clip_ur-0_ut-1_pfe-resp_a-60.json
--- dvc2_train.sh
+++ dvc2_train.sh
@@ -10,3 +10,6 @@ python train.py --config dvc2_tv_sml-60_gop-a_Ife-clip_ur-0_ut-0_pfe-resp_a-90.j
 python train.py --config dvc2_tv_sml-40_gop-a_Ife-clip_ur-0_ut-1_pfe-resp_a-90.json
 
 python train.py --config dvc2_tv_sml-60_gop-60_Ife-clip_ur-0_ut-1_pfe-resp_a-90.json
+
+python train.py --config dvc2_tv_sml-60_gop-a_Ife-clip_ur-0_ut-1_pfe-resp_a-40.json
+python train.py --config dvc2_tv_sml-60_gop-a_Ife-clip_ur-0_ut-1_pfe-resp_a-60.json
