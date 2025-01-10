2a5341579d37eafcd2981c6d7c6e08c2fe5fa194
mohammadjavadpirhadi <mohammadjavad.pirhadi@gmail.com>
2025-01-10 17:11:13 +0330

    fix(download_data): Download only DVC task data

--- prepare_data/download_data.py
+++ prepare_data/download_data.py
@@ -1,31 +1,39 @@
 from SoccerNet.Downloader import SoccerNetDownloader as SNdl
 from getpass import getpass
-import os, zipfile, glob, shutil
+import os, zipfile, glob, shutil, argparse
 from tqdm.auto import tqdm
 
-BASE_DIR="/home/sauleh"
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--base_dir',
+                    required=True,type=str,
+                    help='path to base directory to build data folder')
+
+args = parser.parse_args()
+
+BASE_DIR=args.base_dir
 LOGS_DIR=f"{BASE_DIR}/logs"
 os.makedirs(BASE_DIR, exist_ok=True)
 os.makedirs(LOGS_DIR, exist_ok=True)
 
 mySNdl = SNdl(LocalDirectory=f"{BASE_DIR}/data")
 mySNdl.password = getpass()
-mySNdl.downloadDataTask(
-    task="spotting-ball-2024",
-    split=["train", "valid", "test", "challenge"],
-    password=mySNdl.password
-)
+# mySNdl.downloadDataTask(
+#     task="spotting-ball-2024",
+#     split=["train", "valid", "test", "challenge"],
+#     password=mySNdl.password
+# )
 mySNdl.downloadGames(
     files=["1_224p.mkv", "2_224p.mkv", "Labels-v2.json", "Labels-caption.json", "Labels-ball.json"],
     split=["train","valid","test","challenge"],
     task="caption"
 )
 
-zip_files_dir = glob.glob(f"{BASE_DIR}/data/spotting-ball-2024/*.zip")
+# zip_files_dir = glob.glob(f"{BASE_DIR}/data/spotting-ball-2024/*.zip")
 
-for zip_file_dir in tqdm(zip_files_dir, desc="Extracting spotting ball data"):
-    with zipfile.ZipFile(zip_file_dir, 'r') as zip_file:
-        for file_to_extract in zip_file.namelist():
-            if "720p" not in file_to_extract:
-                zip_file.extract(file_to_extract, f"{BASE_DIR}/data/")
-shutil.rmtree(f"{BASE_DIR}/data/spotting-ball-2024/")
+# for zip_file_dir in tqdm(zip_files_dir, desc="Extracting spotting ball data"):
+#     with zipfile.ZipFile(zip_file_dir, 'r') as zip_file:
+#         for file_to_extract in zip_file.namelist():
+#             if "720p" not in file_to_extract:
+#                 zip_file.extract(file_to_extract, f"{BASE_DIR}/data/")
+# shutil.rmtree(f"{BASE_DIR}/data/spotting-ball-2024/")
